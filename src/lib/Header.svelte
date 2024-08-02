58893362751c1c49ab5679421dda9ecb97bd70f9
cameron <me@cameron.rs>
2024-08-01 22:01:04 -0400

    Update Header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,58 +1,66 @@
 <script lang="ts">
-    import { page } from '$app/stores';
-    
-    const links = [
-        { name: "writing", href: "/blog" },
-        { name: "webasm", href: "https://projects.cameron.rs" },
-        { name: "resume", href: "/resume" },
-    ]
+  import { page } from "$app/stores";
 
-    let page_title: string | null = null;
-    $: {
-        const link = links.find(({ href }) => href.slice(1) === $page.url.pathname.split("/")[1]);
-        if (link) {
-            page_title = link.name.charAt(0).toUpperCase() + link.name.slice(1);
-        } else {
-            page_title = null;
-        }
+  const links = [
+    { name: "blog", href: "/blog" },
+    { name: "linkedin", href: "https://linkedin.com/in/cameronkffn", external: true },
+  ];
+
+  let page_title: string | null = null;
+  $: {
+    const link = links.find(({ href }) => href.slice(1) === $page.url.pathname.split("/")[1]);
+    if (link) {
+      page_title = link.name.charAt(0).toUpperCase() + link.name.slice(1);
+    } else {
+      page_title = null;
     }
+  }
 </script>
 
-<header 
-    class="sticky top-0 z-50 bg-background backdrop-blur-2xl blur-mask backdrop-saturate-[3] supports-[backdrop-filter]:bg-background/60" 
-    data-sveltekit-noscroll 
-    data-sveltekit-preload-code="eager"
+<header
+  class="sticky top-0 z-50 bg-background backdrop-blur-2xl backdrop-saturate-[3] supports-[backdrop-filter]:bg-background/60"
+  data-sveltekit-noscroll
+  data-sveltekit-preload-code="eager"
 >
-    <div class="layout-md py-1 flex justify-between items-center border-b border-neutral-300 dark:border-neutral-700 ">
-        <bi class="my-2 text-xl sm:text-2xl font-livory italic font-bold text-[105%] leading-[100%] text-black dark:text-white">
-            <a href="/">Cameron Kauffman</a>
-            {#if page_title}
-                <span class="font-normal text-neutral-800 dark:text-neutral-200">
-                    <span class="text-neutral-500 hidden sm:inline">—</span>
-                    <p class="sm:inline">
-                        {page_title}
-                    </p>
-                </span>
-            {/if}
-        </bi>
-        <nav class="flex items-start text-neutral-500 justify-end space-x-4 sm:space-x-6 text-lg py-0.5">
-            {#each links as link (link)}
-                <a
-                    href={link.href}
-                    class="hover:dark:text-neutral-100 hover:text-neutral-900 transition-colors"
-                    class:dark:text-white={$page.url.pathname.split("/")[1] === link.href.slice(1)}
-                    class:text-black={$page.url.pathname.split("/")[1] === link.href.slice(1)}
-                >
-                    {link.name}
-                </a>
-            {/each}
-        </nav>
-
-    </div>
+  <div class="layout-md py-1 flex justify-between items-center border-b border-neutral-300 dark:border-neutral-700">
+    <bi
+      class="my-2 text-xl sm:text-2xl font-bold text-black hover:text-black/60 dark:text-white hover:dark:text-white/60"
+    >
+      <a href="/">cameron.rs</a>
+      {#if page_title}
+        <span class="font-normal text-neutral-800 dark:text-neutral-200">
+          <span class="text-neutral-500 hidden sm:inline">—</span>
+          <p class="sm:inline">
+            {page_title}
+          </p>
+        </span>
+      {/if}
+    </bi>
+    <nav class="flex items-start text-neutral-500 justify-end space-x-4 sm:space-x-6 text-lg py-0.5">
+      {#each links as link (link)}
+        <!-- Insane workaround just to make the linkedin one open in a new tab -->
+        {#if link.external}
+          <a
+            href={link.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            class="hover:dark:text-neutral-100 hover:text-neutral-900 transition-colors"
+            class:dark:text-white={$page.url.pathname.split("/")[1] === link.href.slice(1)}
+            class:text-black={$page.url.pathname.split("/")[1] === link.href.slice(1)}
+          >
+            {link.name}
+          </a>
+        {:else}
+          <a
+            href={link.href}
+            class="hover:dark:text-neutral-100 hover:text-neutral-900 transition-colors"
+            class:dark:text-white={$page.url.pathname.split("/")[1] === link.href.slice(1)}
+            class:text-black={$page.url.pathname.split("/")[1] === link.href.slice(1)}
+          >
+            {link.name}
+          </a>
+        {/if}
+      {/each}
+    </nav>
+  </div>
 </header>
-
-<style lang="postcss">
-    .blur-mask {
-        mask-image: linear-gradient(rgba(0,0,0, 1), rgba(0,0,0, 0));
-    }
-</style>
